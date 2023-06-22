43fef0504a913bed37bad8d6ccf59ed5004d7938
Oneshot369 <joshcpeck4@gmail.com>
2023-06-21 20:32:30 -0500

    Added championMasteryDto class for getting champion mastery

---
+++ League-Info-app/Models/ChampionMasteryDto.cs
@@ -0,0 +1,15 @@
+﻿namespace League_Info_app.Models
+{
+	public class ChampionMasteryDto
+	{
+		public long championPointsUntilNextLevel { get; set; }
+		public bool chestGranted { get; set; }
+		public long championId { get; set; }
+        public long lastPlayTime { get; set; }
+        public int championLevel { get; set; }
+        public string summonerId { get; set; }
+        public int championPoints { get; set; }
+        public long championPointsSinceLastLevel { get; set; }
+        public int tokensEarned { get; set; }
+    }
+}
